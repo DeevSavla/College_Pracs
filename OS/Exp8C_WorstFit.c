c64652379f58e5b992a0297b00a2e70109a11ba5
DeevSavla <143065962+DeevSavla@users.noreply.github.com>
2024-05-12 20:47:36 +0530

    Create Exp8C_WorstFit.c

---
+++ OS/Exp8C_WorstFit.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int process[5] = {1, 2, 3, 4,5};
+int incoming[5] = {450,417,112,290,10};
+int memory[5] = {125,500,200,300,600};
+int remaining[5] = {125,500,200,300,600};
+int visited[5] = {0};
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                temp = memory[j];
+                memory[j] = memory[j + 1];
+                memory[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void worstfit()
+{
+    bubbleSort(remaining,5);
+    printf("P\tIncoming\tMemory Block\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("P%d\t%d\t", process[i], incoming[i]);
+        for(int j=0;j<5;j++)
+        {
+            if(incoming[i]<=remaining[j])
+            {
+                printf("%d",memory[j]);
+                printf("\n");
+                remaining[j]-=incoming[i];
+                bubbleSort(remaining,5);
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    
+    worstfit();
+    return 0;
+}
