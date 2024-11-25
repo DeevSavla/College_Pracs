c624997bdb65f1b2988a3614d5c22c92d4407360
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-11-25 18:40:33 +0530

    change exp10 token server

--- CN/exp10/token/servertb.py
+++ CN/exp10/token/servertb.py
@@ -5,48 +5,55 @@ import time
 HOST='127.0.0.1'
 PORT=5001
 MAX_SIZE=5
-LEAK_RATE=2
 
 server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.bind((HOST,PORT))
 server_socket.listen()
 
-print(f"the server socket is listening on: {HOST}:{PORT}...")
 conn,addr=server_socket.accept()
-print(f"connected by {addr} ")
 
 tokens=[]
-incoming_data=[]
-token_lock=threading.Lock()
+received_data=[]
+
+def Leaky():
+    while True:
+        time.sleep(3)
+        if received_data:
+            leaked_data=received_data.pop(0)
+            print(f"the leaked data is {leaked_data}")
+        else:
+            print("nothing there to leak so it is empty")
 
 def generate_tokens():
     while True:
-        with token_lock:
-            if (len(tokens)<MAX_SIZE):
-                tokens.append('*')
-            else:
-                print(f"the bucktet is full.. ")
-        time.sleep(LEAK_RATE)
+        if (len(tokens)<MAX_SIZE):
+            tokens.append('*')
+            print(f"the token bucket looks like this: {tokens}")
+        else:
+            print(f"the bucktet is full.. ")
+        time.sleep(1)
 
 def Data():
     with conn:
         while True:
-            data=conn.recv(1024)
-            if not data or data.decode()=="end":
+            data=conn.recv(1024).decode()
+            if not data or data=="end":
                 if len(tokens)>0:
-                    print(f"the remaining data is: {incoming_data}")
+                    print(f"the remaining tokems is: {tokens}")
                 print("the communication is ended by the client..")
                 break
-            with token_lock:
-                if len(tokens)!=0:
-                    incoming_data.append(data.decode())
-                    tokens.pop(0)
-                    print(f"the incoming data is: {incoming_data}")
-                else:
-                    print("not enough tokens in the bucket so discarding the incoming data.. ")
+            if len(tokens)>0:
+                tokens.pop(0)
+                received_data.append(data)
+                print(f"bucket contains this data: {received_data}")
+            else:
+                print("token bucket is empty discarding the data..")
 
 token_thread=threading.Thread(target=generate_tokens,daemon=True)
 token_thread.start()
+
+leaky_thread=threading.Thread(target=Leaky,daemon=True)
+leaky_thread.start()
 
 Data()
 server_socket.close()
